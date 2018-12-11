499abc78c3e6e9c5030b89c476b0dfbc8d2a205f
Sahil Ayank <sahilayank91@gmail.com>
2018-12-11 20:59:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /****************************************************/
 /*************** Database Connection ****************/
 /****************************************************/
-const dbUrl =
-   'mongodb://sahilayank:woV27rB70hNF4IRr@cluster0-shard-00-00-r1tkp.mongodb.net:27017,cluster0-shard-00-01-r1tkp.mongodb.net:27017,cluster0-shard-00-02-r1tkp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true' ||
+const dbUrl ='mongodb://sahilayank:woV27rB70hNF4IRr@cluster0-shard-00-00-r1tkp.mongodb.net:27017,cluster0-shard-00-01-r1tkp.mongodb.net:27017,cluster0-shard-00-02-r1tkp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true' ||
     `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}/${
         process.env.MONGO_DB
         }`
